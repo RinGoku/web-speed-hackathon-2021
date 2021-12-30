95b9f2506304c2e87c714f9b6079f12704d92e8c
RinGoku <yuyu80105@gmail.com>
2021-12-30 23:48:56 +0900

    fix

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,13 +1,15 @@
 const postcssImport = require('postcss-import');
 const postcssPresetEnv = require('postcss-preset-env');
 const tailwindcss = require('tailwindcss');
+const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
     postcssImport(),
     tailwindcss(),
     postcssPresetEnv({
-      stage: 3,
+      stage: false,
     }),
+    cssnano({ preset: 'default' }),
   ],
 };
